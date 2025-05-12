0efde90950416a3a383ccf10405bf7825466840e
Dhavala Tarun Kumar <60957884+DonTarun27@users.noreply.github.com>
2025-05-12 23:37:15 +0530

    Update ksa_tb.v

--- ksa_tb.v
+++ ksa_tb.v
@@ -4,13 +4,13 @@ module ksa_tst;
     reg Ci;
     wire [15:0] S;
     wire Co;
-  ksa_16bit k1(A, B, Ci, Co, S);		//ksa_nbit #(16) k1(A, B, Ci, Co, S);
+    ksa_16bit k1(.A(A), .B(B), .Ci(Ci), .S(S), .Co(Co));     //ksa_nbit #(.N(16)) k1(.A(A), .B(B), .Ci(Ci), .S(S), .Co(Co));
     initial
     begin A = 16'hFFFF; B = 16'h0001; Ci = 1'b1;
         #10 B = 16'h0019;
         #10 A = 16'h0019; Ci = 1'b0;
         #10 A = 16'hFFFF; B = 16'h0007; Ci = 1'b1;
         #10 B = 16'h0001; Ci = 1'b0;
+        #20 $finish;
     end
-    initial #60 $finish;
 endmodule
